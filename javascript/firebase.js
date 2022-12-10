44312d41d4c9aff1d8df0e35e4363cdf53a0e840
diazcdeveloper <105255678+diazcdeveloper@users.noreply.github.com>
2022-12-10 00:54:20 -0500

    listado y eliminacion
    listado y eliminación con firestore agradados

--- javascript/firebase.js
+++ javascript/firebase.js
@@ -1,6 +1,16 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  import { getDatabase, set, get, update, remove, ref, child } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js"
+  import {
+    getFirestore,
+    onSnapshot,
+    doc,
+    collection,
+    deleteDoc,
+    getDocs,
+    getDoc,
+    addDoc,
+  } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+  // Import the functions you need from the SDKs you need
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,65 +27,17 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const db = getDatabase();
+  const db = getFirestore();
 
+  export const agregarAgenda =() => addDoc(collection(db, 'agenda'),{
+    mes: mes.value,
+    coordinador: coordinador.value,
+    alabanza: alabanza.value,
+    ujier: ujier.value,
+    maestro: maestro.value
+  })
 
-const agendaId = document.getElementById("agenda-id");
-const mes = document.getElementById("mes");
-const coordinador = document.getElementById("coordinador");
-const alabanza = document.getElementById("alabanza");
-const ujier = document.getElementById("ujier");
-const maestro = document.getElementById("maestro");
-const btnAgregar = document.getElementById("btn-agregar");
+  export const listarAgenda = (callback) => onSnapshot(collection(db, 'agenda'), callback);
 
-const agendaIdShow = document.getElementById("agenda-id-show");
-const mesShow = document.getElementById("mes-show");
-const coordinadorShow = document.getElementById("coordinador-show");
-const alabanzaShow = document.getElementById("alabanza-show");
-const ujierShow = document.getElementById("ujier-show");
-const maestroShow = document.getElementById("maestro-show");
-const btnPublicar = document.getElementById("btn-publicar");
-
-function insertData (){
-    set(ref(db, "agenda/" + agendaId.value),{
-        id: agendaId.value,
-        mes: mes.value,
-        coordinador: coordinador.value,
-        alabanza: alabanza.value,
-        ujier: ujier.value,
-        maestro: maestro.value
-
-    })
-    .then(()=>{
-        alert("agenda agregada!")
-    })
-    .catch((error)=>{
-        alert(error)
-    })
-}
-
-function findData (){
-    const dbref = ref(db);
-
-    get(child(dbref, "agenda/" + agendaIdShow.value))
-    .then((snapshot)=>{
-        if(snapshot.exists()){
-            mesShow.innerHTML = snapshot.val().mes;
-            coordinadorShow.innerHTML = snapshot.val().coordinador;
-            alabanzaShow.innerHTML = snapshot.val().alabanza;
-            ujierShow.innerHTML = snapshot.val().ujier;
-            maestroShow.innerHTML = snapshot.val().maestro;
-        }else{
-            alert("no se encontró esa agenda");
-        }
-    })
-    .catch((error)=>{
-        alert(error)
-    })
-
-}
-
-
-btnAgregar.addEventListener("click", insertData);
-btnPublicar.addEventListener("click", findData);
+  export const eliminarAgenda  = (id) => deleteDoc(doc(db, 'agenda', id));
 
